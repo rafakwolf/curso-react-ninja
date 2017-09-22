ae65c062fc01f72310e1e501a84128bfa207d19a
Rafael Kaciano Wolf <rafakwolf@outlook.com>
2017-09-22 20:31:56 -0300

    jsx + babel

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 'use strict'
 
-var React = require('react');
+import React from 'react';
 
 var Title = React.createClass({
     render: function () {
-        return React.createElement('h1', null, 'Título');
+        return <h1>Título</h1>
     }
 })
 
-module.exports = Title;
+export default Title;
